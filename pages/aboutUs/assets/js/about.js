b96bdd552a683d155de1f6dd2d6ae987e4c01c54
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-03-16 17:31:13 -0700

    About us page responsiveness ok.

---
+++ pages/aboutUs/assets/js/about.js
@@ -0,0 +1,23 @@
+const dropButton = document.querySelector('#toggle-button');
+const dropContent = document.querySelector('.dropContent');
+const footerMobile = document.querySelector('#footer');
+
+
+
+dropButton.addEventListener('click', () => {
+    if (dropContent.style.display === 'flex') {
+        dropContent.style.display = 'none';
+        dropButton.style.transform = 'rotate(0deg) translateZ(0)';
+        dropButton.classList.remove('transform2')
+        dropButton.classList.remove('transform1')
+        return;
+    } else {
+        dropContent.style.display = 'flex';
+        dropButton.style.transform = 'rotate(-90deg) translateZ(0)';
+        dropButton.classList.add('transform2')
+        dropButton.classList.add('transform1')
+        return;
+    }
+})
+
+
